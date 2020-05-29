f1e61b9aeeac2c67e042e9527e4bf5d93003736b
kmeersman <katie4682@yahoo.com>
2020-05-28 18:56:47 -0600

    finished readme generator

--- index.js
+++ index.js
@@ -1,59 +1,112 @@
-const inquirer = require("inquirer");
+//Node Modules
 const fs = require("fs");
+const util = require("util");
 
-const questions = [
+//NPM Packages
+const inquirer = require("inquirer");
 
-];
+const path = require("path");
+
+function writeFile(filename, data) {
+  return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
+// const generateMarkdown = require("./generateMarkdown");
+
 inquirer
-.prompt([
+  .prompt([
     {
-        name: "title",
-        type: "input",
-        message: "What is the title of your project?",
+      name: "title",
+      type: "input",
+      message: "What is the title of your project?",
     },
     {
-        name: "description",
-        type: "input",
-        message: "Waht is the description of the project?",
+      name: "description",
+      type: "input",
+      message: "What is the description of the project?",
     },
     {
-        name: "contents",
-        type: "list",
-        message: "",
+      name: "installation",
+      type: "input",
+      message: "What are the steps required to install your project?",
     },
     {
-        name: "usage",
-        type: "input",
-        message: "",
+      name: "license",
+      type: "input",
+      message: "What is your license?",
     },
     {
-        name: "license",
-        type: "input",
-        message: "",
-    },    
+      name: "contributing",
+      type: "input",
+      message: "Who are the contributors on this project?",
+    },
     {
-        name: "contributing",
-        type: "input",
-        message: "",
-    },    
+      name: "github",
+      type: "input",
+      message: "What is your GitHub username?",
+    },
     {
-        name: "tests",
-        type: "input",
-        message: "",
-    },  
+      name: "repoName",
+      type: "input",
+      message: "What is your GitHub repo name?",
+    },
     {
-        name: "questions",
-        type: "input",
-        message: "",
-    },  
+      name: "usage",
+      type: "input",
+      message: "Provide instructions for use of your project",
+    },
+    {
+      name: "tests",
+      type: "input",
+      message: "Insert Examples of tests",
+    },
+    {
+      name: "question1",
+      type: "input",
+      message: "What is your GitHub proflie picture?",
+    },
+    {
+      name: "question2",
+      type: "input",
+      message: "What is your GitHub email??",
+    },
+  ])
+  .then(function (user) {
+    console.log(user);
+    const template = `
+  # ${user.title}
+  ## Description
+  ${user.description}
 
-    
-])
-function writeToFile(fileName, data) {
-}
+  ![GitHub repo size](https://img.shields.io/github/repo-size/${user.github}/${user.repoName})
 
-function init() {
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions) 
 
-}
+  ## Installation
+  ${user.installation}
 
-init();
+  ## Usage
+  ${user.usage}
+
+  ## License
+  ${user.license} 
+
+  ![GitHub](https://img.shields.io/github/license/${user.github}/${user.repoName})
+
+  ## Contributing
+  ${user.contributing}
+
+  ## Tests
+
+  ## Questions
+  * ${user.question1}
+  * ${user.question2}
+
+    `;
+    writeFile("README.md", template);
+  });
